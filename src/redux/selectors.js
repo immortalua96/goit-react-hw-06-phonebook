3e5d6ce18e057ede4b691ef525d033a5843b72a9
immortalua96 <romanshvagla@icloud.com>
2023-08-15 13:59:43 +0200

    fix

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,14 +1,24 @@
+import { createSelector } from "@reduxjs/toolkit";
 export const getContacts = ({ contacts: { contacts } }) => contacts;
 
 export const getFilter = ({ filter }) => filter;
 
-export const getFilteredContacts = ({ contacts: { contacts }, filter }) => {
-  if (!filter) {
-    return contacts;
-  }
-  const normalizedFilter = filter.toLowerCase();
+export const getFilteredContacts = createSelector(
+  [getContacts, getFilter],
+  (contacts, filter) => {
+   
+
+    switch (filter) {
+      case !filter:
+        return contacts;
+      case filter:
+        const normalizedFilter = filter.toLowerCase();
 
   return contacts.filter(contact =>
     contact.name.toLowerCase().includes(normalizedFilter)
   );
-};
+      default:
+        return contacts;
+    }
+  }
+);
